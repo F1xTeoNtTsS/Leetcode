fd449ff5220037d1416838889992f305239703fa
F1xTeoNtTsS <f1xteonttss@F1xTeoNtTsSs-MacBook-Pro.local>
2022-12-15 18:53:18 +0800

    new mac test

--- Problems/Problems/main.swift
+++ Problems/Problems/main.swift
@@ -7,13 +7,4 @@
 
 import Foundation
 
-var matrix1 = [[1,1,1],[1,0,1],[1,1,1]]
-print(matrix1)
-setZeroes(&matrix1)
-print(matrix1)
-
-
-var matrix2 = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
-print(matrix2)
-setZeroes(&matrix2)
-print(matrix2)
+// Call your method here
