3c8178c53b867d0782d42a91f2545c48cd900996
F1xTeoNtTsS <f1xteonttss@gmail.com>
2022-12-23 17:23:37 +0800

    reverseList

---
+++ Problems/Problems/Linked List/LinkedList.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedList.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 23/12/2022.
+//
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
---
+++ Problems/Problems/Linked List/reverseList.swift
@@ -0,0 +1,26 @@
+//
+//  reverseList.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 23/12/2022.
+//
+
+import Foundation
+
+func reverseList(_ head: ListNode?) -> ListNode? {
+    var prev: ListNode? = nil
+    var cur = head
+    
+    while cur?.next != nil {
+        let next = cur?.next
+        cur?.next = prev
+        prev = cur
+        cur = next
+    }
+    return prev
+}
+
+// https://leetcode.com/problems/reverse-linked-list
+// MARK: - Cases
+// [1,2,3,4,5] -> [5,4,3,2,1]
+// [1,2] -> [2,1]
