a00a85e02d0b64c5a442934bb4d54224b09cbe09
F1xTeoNtTsS <f1xteonttss@gmail.com>
2023-04-18 15:37:44 +0700

    add some problems

---
+++ Problems/Problems/Trees/hasPathSum.swift
@@ -0,0 +1,19 @@
+//
+//  hasPathSum.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 17/04/2023.
+//
+
+import Foundation
+
+func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+    guard let root = root else { return false }
+
+    if root.val == targetSum && root.left == nil && root.right == nil { return true }
+
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+}
+
+// https://leetcode.com/problems/path-sum/
+
---
+++ Problems/Problems/Trees/maxDepth.swift
@@ -0,0 +1,15 @@
+//
+//  maxDepth.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 17/04/2023.
+//
+
+import Foundation
+
+
+func maxDepth(_ root: TreeNode?) -> Int {
+    guard let root = root else { return 0 }
+
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
+}
