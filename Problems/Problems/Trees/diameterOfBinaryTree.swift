45b04057df624aedb71a76260dc30da5b8fbed64
F1xTeoNtTsS <f1xteonttss@gmail.com>
2023-04-19 18:46:45 +0700

    add some problems

---
+++ Problems/Problems/Trees/diameterOfBinaryTree.swift
@@ -0,0 +1,27 @@
+//
+//  diameterOfBinaryTree.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 18/04/2023.
+//
+
+import Foundation
+
+func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+    guard root != nil else { return 0 }
+
+    func dfs(_ node: TreeNode?, _ diameter: inout Int) -> Int {
+        guard let node = node else { return 0 }
+
+        let left = dfs(node.left, &diameter)
+        let right = dfs(node.right, &diameter)
+
+        diameter = max(diameter, left + right)
+
+        return 1 + max(left, right)
+    }
+
+    var diameter = -1
+    _ = dfs(root, &diameter)
+    return diameter
+}
