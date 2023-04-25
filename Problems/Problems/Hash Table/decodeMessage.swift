99d517d5a59b5494e7bac48dab04952c69556e29
F1xTeoNtTsS <f1xteonttss@gmail.com>
2023-04-25 15:27:19 +0500

    decodeMessage

---
+++ Problems/Problems/Hash Table/decodeMessage.swift
@@ -0,0 +1,32 @@
+//
+//  decodeMessage.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 25/04/2023.
+//
+
+import Foundation
+
+func decodeMessage(_ key: String, _ message: String) -> String {
+    let alphabet: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var dict: [Character: Character] = [:]
+
+    var index = 0
+
+    for k in key {
+        if k.isLetter && dict[k] == nil {
+            dict[k] = alphabet[index]
+            index += 1
+        }
+    }
+
+    var result = ""
+
+    for l in message {
+        let a = dict[l] ?? l
+        result.append(a)
+
+    }
+    return result
+}
