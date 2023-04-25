699669e15528acc051b716b9d3fffce9a82b1f08
F1xTeoNtTsS <f1xteonttss@gmail.com>
2023-04-25 15:40:34 +0500

    checkIfPangram

---
+++ Problems/Problems/Hash Table/checkIfPangram.swift
@@ -0,0 +1,19 @@
+//
+//  checkIfPangram.swift
+//  Problems
+//
+//  Created by F1xTeoNtTsS on 25/04/2023.
+//
+
+import Foundation
+
+func checkIfPangram(_ sentence: String) -> Bool {
+    var alphabet: [Character: String] = ["a": "", "b": "", "c": "", "d": "", "e": "", "f": "", "g": "",
+                                         "h": "", "i": "", "j": "", "k": "", "l": "", "m": "", "n": "",
+                                         "o": "", "p": "", "q": "", "r": "", "s": "", "t": "", "u": "",
+                                         "v": "", "w": "", "x": "", "y": "", "z": ""]
+
+    sentence.forEach { alphabet[$0] = nil }
+
+    return alphabet.isEmpty
+}
